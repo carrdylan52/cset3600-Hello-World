766cd57293a584d134f6d1f25818bd98bdb09ffd
Dylan Carr <carrdylan52@gmail.com>
2024-02-03 14:33:28 -0500

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,10 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+// Name: Dylan Carr
+// Class: CSET 3600
+// Instructor: Don Miller
+// Homework: 1
+// Date: 020324
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
         System.out.print("Hello World!");
     }
 }
